604928d3b408365823cd6341b07d2f2e68bbff81
Oscar Losada <oscarlosu@gmail.com>
2015-10-28 12:38:03 +0100

    Initial commit.

---
+++ GameDesignProject/Assets/Scripts/Module.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using GamepadInput;
+
+public abstract class Module : MonoBehaviour
+{
+    public float Weight;
+    public GamePad.Button Button;
+    public GamePad.Index Controller;
+    public List<GameObject> Sockets;
+
+    public abstract void Activate();
+
+    public void Update()
+    {
+        if (GamePad.GetButtonDown(Button, Controller))
+        {
+            Activate();
+        }
+    }
+}
---
+++ GameDesignProject/Assets/Scripts/Thruster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thruster : Module
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public override void Activate()
+    {
+
+    }
+}
