97ef21adc5c340733ff6743e03d910aa6a26f46b
MaxWrighton93 <MaxWrighton93@live.co.uk>
2015-11-11 18:14:34 +0100

    wrap around for the nebula background

--- GameDesignProjectClean/Assets/Scripts/NebulaParallax.cs
+++ GameDesignProjectClean/Assets/Scripts/NebulaParallax.cs
@@ -8,13 +8,26 @@ public class NebulaParallax : MonoBehaviour
     public Transform gameCamera;
     [Space(10)]
     public float parallaxModifier;
+    public float xWrapAroundBuffer;
+    public float yWrapAroundBuffer;
 
     Vector3 prevCamPos;
+    Transform[] nebulaArray;
+    float numOfNubulas;
+    float xNebSize;
+    float yNebSize;
 
     // Use this for initialization
     void Start()
     {
         prevCamPos = gameCamera.position;
+        nebulaArray = GetComponentsInChildren<Transform>();
+        InvokeRepeating("CheckBackground", 0.1f, 0.1f);
+        xNebSize = transform.localScale.x;
+        yNebSize = transform.localScale.y;
+        numOfNubulas = Mathf.Sqrt((float)nebulaArray.Length-1f);
+        
+
     }
 
     // Update is called once per frame
@@ -28,5 +41,38 @@ public class NebulaParallax : MonoBehaviour
         }
 
         prevCamPos = gameCamera.position;
+    }
+
+    void CheckBackground()
+    {
+        float xOffset;
+        float yOffset;
+        for(int i = 0; i < nebulaArray.Length; i++)
+        {
+            if (nebulaArray[i].name == "Nebula")
+                continue;
+
+            //print("out of the if");
+            xOffset = gameCamera.position.x - nebulaArray[i].position.x;
+            yOffset = gameCamera.position.y - nebulaArray[i].position.y;
+
+            if(xOffset > xWrapAroundBuffer)
+            {
+                transform.position += transform.right * xNebSize * numOfNubulas;
+            }
+            else if(xOffset < -xWrapAroundBuffer)
+            {
+                transform.position -= transform.right * xNebSize * numOfNubulas;
+            }
+
+            if (yOffset > yWrapAroundBuffer)
+            {
+                transform.position += transform.up * yNebSize * numOfNubulas;
+            }
+            else if (yOffset < -yWrapAroundBuffer)
+            {
+                transform.position -= transform.up * yNebSize * numOfNubulas;
+            }
+        }
     }
 }
