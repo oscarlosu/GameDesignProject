e28ab1e16b82cee810202aa7f5aff2d5b0438720
Phillip Phoelich <pphoelich@gmail.com>
2015-11-16 19:35:59 +0100

    Working on the new thruster system, that moves the ship in the direction of the stick.

--- GameDesignProjectClean/Assets/Scripts/Components/Thruster.cs
+++ GameDesignProjectClean/Assets/Scripts/Components/Thruster.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using GamepadInput;
 using UnityEditor;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(AudioSource))]
 public class Thruster : Module
@@ -79,6 +81,140 @@ public class Thruster : Module
         if (leftStickValue.magnitude > 0)
         {
             float powerX = 0, powerY = 0;
+
+            // Is ship's direction the direction we want?
+            float dot = Vector3.Dot(new Vector3(leftStickValue.x, leftStickValue.y), Core.transform.up);
+            float crossZ = Vector3.Cross(new Vector3(leftStickValue.x, leftStickValue.y), Core.transform.up).z;
+
+            var shipRelative = Core.transform.InverseTransformPoint(transform.position);
+            float dotThruster = Vector3.Dot(Core.transform.up, transform.up);
+            float crossZThruster = Vector3.Cross(Core.transform.up, transform.up).z;
+
+            if (dot > 1 - float.Epsilon) // If it is, power all forwards thrusters.
+            {
+                // If thruster is pointing backwards, fire.
+                if (dotThruster < -0.01) // Thruster pointing backwards.
+                {
+                    powerX = ThrustPower;
+                    //powerY = ThrustPower;
+                }
+
+            }
+            else if (dot < -1 + float.Epsilon) // Go backwards (possibly turn as well?).
+            {
+                // If thruster is pointing forwards, fire.
+                if (dotThruster > 0.01)
+                {
+                    powerX = ThrustPower;
+                    //powerY = leftStickValue.y;
+                }
+            }
+            else if (crossZ > 0) // Turn left.
+            {
+                // If moving forwards, fire thrusters pointing backwards on the right side and thrusters pointing right on the upper right side and pointing left on the lower left.
+                if (dot > float.Epsilon)
+                {
+                    // Thrusters pointing backwards.
+                    if (dotThruster < -0.01 && shipRelative.x > 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                    // Thrusters pointing right in the upper right side.
+                    else if (crossZThruster < 0 && shipRelative.x > 0 && shipRelative.y > 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                    // Thrusters pointing left in the lower left side.
+                    else if (crossZThruster > 0 && shipRelative.x < 0 && shipRelative.y < 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                }
+                // If moving backwards, fire thrusters pointing forwards on the right side and thrusters pointing right on the lower right side and pointing left on the upper left.
+                else if (dot < float.Epsilon)
+                {
+                    // Thrusters pointing forwards.
+                    if (dotThruster > 0.01 && shipRelative.x > 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                    // Thrusters pointing right in the lower right side.
+                    else if (crossZThruster < 0 && shipRelative.x > 0 && shipRelative.y < 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                    // Thrusters pointing left in the upper left side.
+                    else if (crossZThruster > 0 && shipRelative.x < 0 && shipRelative.y > 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                }
+                // If not moving forwards or backwards, but just rotating, fire thrusters pointing right on upper right side and thrusters pointing left on lower left side.
+                else if (Mathf.Abs(dot) < float.Epsilon)
+                {
+                    // Thrusters pointing right in upper right corner.
+                    if (crossZThruster < 0 && shipRelative.x > 0 && shipRelative.y > 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                    else if (crossZThruster > 0 && shipRelative.x < 0 && shipRelative.y < 0)
+                    {
+                        powerX = ThrustPower;
+                        //powerY = ThrustPower;
+                    }
+                }
+
+            }
+            else if (crossZ < 0) // Turn right. // TODO I'M RIGHT HERE IN THE SEARCH FOR BETTER MOVEMENT CONTROLS!
+            {
+                // If moving forwards, fire thrusters pointing backwards on the left side and thrusters pointing left on the upper left side and pointing right on the lower right.
+                if (dot > float.Epsilon)
+                {
+
+                }
+                // If moving backwards, fire thrusters pointing forwards on the left side and thrusters pointing left on the lower left side and pointing right on the upper right.
+                else if (dot < float.Epsilon)
+                {
+
+                }
+                // If not moving forwards or backwards, but just rotating, fire thrusters pointing left on upper left side and thrusters pointing right on lower right side..
+                else if (Mathf.Abs(dot) < float.Epsilon)
+                {
+
+                }
+            }
+
+            var powerTotal = powerX + powerY;
+            // DEBUG: Change sprite colour, when power is larger than 0.
+            GetComponent<SpriteRenderer>().color = powerTotal > 0 ? Color.magenta : Color.white;
+            Core.GetComponent<Rigidbody2D>().AddForceAtPosition((-transform.up) * ThrustPower * (powerTotal), transform.position);
+            if (childParticles.Length > 0)
+            {
+                if (powerTotal > 0 && !childParticles[0].isPlaying)
+                {
+                    for (int i = 0; i < childParticles.Length; i++)
+                    {
+                        childParticles[i].Play();
+                    }
+                }
+            }
+
+
+            // If not, should ship turn left or right.
+
+            // If turning right, Fire all forward thrusters in lower left quadrant
+            // and all thrusters pointing left in upper left quadrant.
+
+
+
+            /*float powerX = 0, powerY = 0;
             var shipRelative = Core.transform.InverseTransformPoint(transform.position);
 
             // Determines if thrusters point forwards, backwards or sideways.
@@ -267,7 +403,7 @@ public class Thruster : Module
                         childParticles[i].Play();
                     }
                 }
-            }
+            }*/
         }
         else
         {
