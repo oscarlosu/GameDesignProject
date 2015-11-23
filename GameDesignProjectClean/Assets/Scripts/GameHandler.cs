57264cfa1a565c93268df3074260f6fef7c79929
Phillip Phoelich <pphoelich@gmail.com>
2015-11-23 20:07:44 +0100

    Switching between player select and level select now works and settings are saved in GameHandler object, which is persistent. You can switch to the builder scene, but nothing is done there yet.

--- GameDesignProjectClean/Assets/Scripts/GameHandler.cs
+++ GameDesignProjectClean/Assets/Scripts/GameHandler.cs
@@ -3,13 +3,34 @@ using GamepadInput;
 
 public class GameHandler : MonoBehaviour
 {
+    private static GameObject gameHandler;
 
     public Scene CurrentScene;
-    public GamePad.Button BackButton; 
+    public GamePad.Button BackButton;
+
+    // Scene handlers.
+    public LevelHandler LevelHandler;
+    public PlayerSelectHandler PlayerSelectHandler;
+
+    // Game setup settings.
+    private bool[] playersJoined;
+    private string levelSelectedSceneName;
 
     public enum Scene
     {
         PlayerSelectScene, LevelSelectScene, BuilderScene, GameScene
+    }
+
+    private void Awake()
+    {
+        if (gameHandler != null)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        else
+        {
+            gameHandler = this.gameObject;
+        }
     }
 
 	// Use this for initialization
@@ -19,9 +40,35 @@ public class GameHandler : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+	    GotoPrevScene(); // Check if going back.
+	    GotoNextScene(); // Check if going forward.
 	}
+
+    public void GotoPrevScene()
+    {
+        switch (CurrentScene)
+        {
+            case Scene.PlayerSelectScene:
+                // Can't go further back... Don't do anything.
+                return;
+            case Scene.LevelSelectScene:
+                // Go to PlayerSelectScene and reset level select values.
+                if (GamePad.GetButtonDown(BackButton, GamePad.Index.Any))
+                {
+                    Application.LoadLevel("PlayerSelect");
+                    CurrentScene = Scene.PlayerSelectScene;
+                }
+                return;
+            case Scene.BuilderScene:
+                // Go to LevelSelectScene and reset builder values.
+                return;
+            case Scene.GameScene:
+                // Go back to the PlayerSelectScene and reset all values.
+                return;
+        }
+    }
 
     public void GotoNextScene()
     {
@@ -29,16 +76,39 @@ public class GameHandler : MonoBehaviour
         {
             case Scene.PlayerSelectScene:
                 // Check if there are more than one player before going to the next scene.
-                break;
+                if (GamePad.GetButtonDown(GamePad.Button.Start, GamePad.Index.Any))
+                {
+                    if (PlayerSelectHandler.PlayersJoined(out playersJoined))
+                    {
+                        Debug.Log(playersJoined);
+                        Application.LoadLevel("LevelSelect");
+                        CurrentScene = Scene.LevelSelectScene;
+                    }
+                    else
+                    {
+                        Debug.LogError("At least two players are needed.");
+                    }
+                }
+                return;
             case Scene.LevelSelectScene:
                 // Level should be selected before progressing to the next scene.
-                break;
+                if (GamePad.GetButtonDown(GamePad.Button.Start, GamePad.Index.Any))
+                {
+                    if (LevelHandler != null)
+                    {
+                        // Save the selected scene name.
+                        levelSelectedSceneName = LevelHandler.GetSelectedLevelSceneName();
+                        Application.LoadLevel("Builder");
+                        CurrentScene = Scene.BuilderScene;
+                    }
+                }
+                return;
             case Scene.BuilderScene:
                 // All players should have declared themselves ready.
-                break;
+                return;
             case Scene.GameScene:
                 // The winning condition should have been met.
-                break;
+                return;
         }
     }
 }
