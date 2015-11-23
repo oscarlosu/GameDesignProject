e5f256c7d2340a2b2823d59ee48b3f20f4825dc6
MaxWrighton93 <MaxWrighton93@live.co.uk>
2015-11-23 18:25:45 +0100

    yeah i have no idea what you guys are saying

--- GameDesignProjectClean/Assets/Scripts/Components/Implosion.cs
+++ GameDesignProjectClean/Assets/Scripts/Components/Implosion.cs
@@ -2,23 +2,37 @@
 using System.Collections;
 
 public class Implosion : MonoBehaviour
-{	
-	public float PullForce;
+{
+    public float scaleFactor = 1.0f;
+    public float duration = 1.0f;
+    public AnimationCurve explosionAnim = new AnimationCurve(new Keyframe(0, 0.05f), new Keyframe(0.95f, 1), new Keyframe(1, 0.05f));
+    [Space(10)]
+    public float PullForce;
+    public Material[] implosionMats;
 
-	private Animator anim;
+    private float counter = 0.0f;
+
 	void Awake ()
 	{
-		//GetComponent<AudioSource> ().pitch = Random.Range (0.5f, 1.5f);
-	}
+        //GetComponent<AudioSource> ().pitch = Random.Range (0.5f, 1.5f);
+        Material mat = implosionMats[Random.Range(0, implosionMats.Length)];
+        GetComponent<MeshRenderer>().material = mat;
+        StartCoroutine("DestroyImplosion");
+    }
 
 	void Start()
 	{
-		anim = GetComponent<Animator>();
-		ParticleManager.Instance.SpawnParticle(ParticleType.Implosion, transform.position);
 
 	}
-	
-	private void OnTriggerExit2D (Collider2D other)
+
+    void Update()
+    {
+        float newScale = explosionAnim.Evaluate(counter) * scaleFactor;
+        transform.localScale = Vector3.one * newScale;
+        counter += Time.deltaTime / duration;
+    }
+
+    private void OnTriggerExit2D (Collider2D other)
 	{
 		if (other.attachedRigidbody != null) {
 			// Calculate pull force direction
@@ -28,8 +42,9 @@ public class Implosion : MonoBehaviour
 		}
 	}
 	
-	private void DestroyImplosion ()
+	IEnumerator DestroyImplosion ()
 	{
+        yield return new WaitForSeconds(duration);
 		GameObject.Destroy (gameObject);
 	}
 }
--- GameDesignProjectClean/Assets/Scripts/Components/Pulse.cs
+++ GameDesignProjectClean/Assets/Scripts/Components/Pulse.cs
@@ -1,26 +1,41 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Pulse : Projectile
 {
-	public float RepulsionForce;
+    public float scaleFactor = 1.0f;
+    public float duration = 1.0f;
+    public AnimationCurve explosionAnim = new AnimationCurve(new Keyframe(0, 0.05f), new Keyframe(1, 1));
+    [Space(10)]
+    public float RepulsionForce;
 	public float TorqueMagnitude;
 	public float Radius;
 	public float Speed;
 
 	private CircleCollider2D col;
-	// Use this for initialization
-	void Start ()
+    public Material[] pulseMats;
+
+    private float counter = 0.0f;
+    private Material mat;
+    // Use this for initialization
+    void Start ()
 	{
 		col = GetComponent<CircleCollider2D>();
-		ParticleManager.Instance.SpawnParticle(ParticleType.Pulse, transform.position);
-		InGrace = true;
-	}
+        mat = pulseMats[Random.Range(0, pulseMats.Length)];
+        GetComponent<MeshRenderer>().material = mat;
+        mat = GetComponent<MeshRenderer>().material;
+        InGrace = true;
+        StartCoroutine("DestroyPulse");
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
-	}
+        mat.color  = new Color(mat.color.r, mat.color.g, mat.color.b, 1 - explosionAnim.Evaluate(counter));
+        float newScale = explosionAnim.Evaluate(counter) * scaleFactor;
+        transform.localScale = Vector3.one * newScale;
+        counter += Time.deltaTime / duration;
+    }
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
@@ -39,8 +54,9 @@ public class Pulse : Projectile
 		}
 	}
 
-	private void DestroyPulse ()
+	IEnumerator DestroyPulse ()
 	{
+        yield return new WaitForSeconds(duration);
 		GameObject.Destroy (gameObject);
 	}
 }
