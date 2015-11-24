089c75bc128cfe871e296b11e2704d849d24e75e
Phillip Phoelich <pphoelich@gmail.com>
2015-11-24 15:49:25 +0100

    Full flow should now work. From player select to game.

--- GameDesignProjectClean/Assets/Scripts/BuilderHandler.cs
+++ GameDesignProjectClean/Assets/Scripts/BuilderHandler.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class BuilderHandler : MonoBehaviour
 {
+    public GameObject[] PlayerAreas;
 
     public delegate void PreviousScene();
     private PreviousScene prevScene;
+    private GameObject[] ships = new GameObject[4];
+    private bool[] playersJoined;
 
     // Use this for initialization
     void Start()
@@ -16,6 +20,8 @@ public class BuilderHandler : MonoBehaviour
         {
             gameHandler.GetComponent<GameHandler>().BuilderHandler = this;
             prevScene = gameHandler.GetComponent<GameHandler>().GotoPrevScene;
+            playersJoined = gameHandler.GetComponent<GameHandler>().GetPlayersJoined();
+            ActivatePlayerAreas(playersJoined);
         }
     }
 
@@ -25,9 +31,37 @@ public class BuilderHandler : MonoBehaviour
 
     }
 
+    // Activating the player areas for the players, who joined.
+    private void ActivatePlayerAreas(bool[] players)
+    {
+        for (var i = 0; i < players.Length; i++)
+        {
+            if (players[i])
+            {
+                PlayerAreas[i].SetActive(true);
+            }
+        }
+    }
+
     public bool PlayersReady(out GameObject[] playerShips)
     {
+        // If the number of players is equal to the number of ships marked ready. Everyone are ready.
+        var nbPlayers = playersJoined.Count(player => player);
+        var nbShips = ships.Count(ship => ship != null);
+        if (nbPlayers == nbShips)
+        {
+            playerShips = ships;
+            return true;
+        }
         playerShips = null;
-        return true;
+        return false;
+    }
+
+    public void SetPlayerShip(int playerIndex, GameObject ship)
+    {
+        ship.transform.parent = null;
+        DontDestroyOnLoad(ship);
+        ships[playerIndex] = ship;
+        Debug.Log("Player: " + (playerIndex + 1) + " finished: " + ship);
     }
 }
