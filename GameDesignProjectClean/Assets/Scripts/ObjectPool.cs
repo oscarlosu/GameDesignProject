4742ed9bccb7cd01cf62c3c8bec93cf4514ceeb3
Oscar Losada <oscarlosu@gmail.com>
2015-11-25 19:10:06 +0100

    Implemented ObjectPool and created prefab.

---
+++ GameDesignProjectClean/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour
+{
+	public List<GameObject> ObjectPrefabs;
+	public List<int> ObjectPoolsDefaultSize;
+	public int HardSizeLimit;
+	private Dictionary<ObjectType, Stack<GameObject>> objectPools;
+	private int objectCount;
+
+	public enum ObjectType
+	{
+		Rocket = 0,
+		Missile = 1,
+		Mine = 2,
+		ImplosionBomb = 3,
+		Explosion = 4,
+		Implosion = 5,
+		Pulse = 6,
+		Laser = 7
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		/*System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+		watch.Start ();*/
+		// Initialize pools
+		objectPools = new Dictionary<ObjectType, Stack<GameObject>>();
+		objectCount = 0;
+		foreach(ObjectType type in System.Enum.GetValues(typeof(ObjectType)))
+		{
+			objectPools.Add(type, new Stack<GameObject>());
+			// Initialize with the default amount of game objects
+			for(int i = 0; i < ObjectPoolsDefaultSize[(int)type]; ++i)
+			{
+				GameObject obj = Instantiate (ObjectPrefabs[(int)type]);
+				obj.SetActive(false);
+				obj.transform.parent = transform;
+				objectPools[type].Push (obj);
+			}
+			objectCount += ObjectPoolsDefaultSize[(int)type];
+		}
+		/*watch.Stop ();
+		Debug.Log ("Pool initialization required: " + watch.ElapsedMilliseconds + " ms");
+		Debug.Log ("ObjectCount: " + objectCount);*/
+		InvokeRepeating("ReportStatus", 0, 10);
+
+	}
+
+	public GameObject RequestPoolObject(ObjectType type)
+	{
+		if(objectPools[type].Count > 0)
+		{
+			GameObject obj = objectPools[type].Pop();
+			obj.SetActive(true);
+			return obj;
+		}
+		else if (objectCount < HardSizeLimit)
+		{
+			GameObject obj = Instantiate (ObjectPrefabs[(int)type]);
+			obj.transform.parent = transform;
+			++objectCount;
+			return obj;
+		}
+		else
+		{
+			throw new System.Exception("Object Pool hard size limit reached. No more pool objects will be instantiated.");
+		}
+	}
+
+	public void DisablePoolObject(GameObject obj, ObjectType type)
+	{
+		// Disable the object
+		obj.SetActive(false);
+		// Put it in the pool for this kind of object
+		objectPools[type].Push(obj);
+	}
+
+	private void ReportStatus()
+	{
+		string status = "Pool status:\n";
+		foreach(KeyValuePair<ObjectType, Stack<GameObject>> pair in objectPools)
+		{
+			status += pair.Key.ToString() + " pool has " + pair.Value.Count + " objects\n";
+		}
+		Debug.LogFormat(status);
+	}
+}
