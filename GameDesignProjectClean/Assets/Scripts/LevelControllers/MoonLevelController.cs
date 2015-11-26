9f9827cc8b34dd25f110bd4d5a03ab49a1c861e9
Phillip Phoelich <pphoelich@gmail.com>
2015-11-26 19:13:46 +0100

    Worked on some UI in the builder scene. Needs updates.

--- GameDesignProjectClean/Assets/Scripts/LevelControllers/MoonLevelController.cs
+++ GameDesignProjectClean/Assets/Scripts/LevelControllers/MoonLevelController.cs
@@ -21,19 +21,19 @@ public class MoonLevelController : MonoBehaviour, ILevelHandler
         if (playerShips[0] != null)
         {
             playerShips[0].transform.position = new Vector3(-100, 20);
-            playerShips[3].transform.eulerAngles = new Vector3(0, 0, 315);
+            playerShips[0].transform.eulerAngles = new Vector3(0, 0, 45);
             playerShips[0].SetActive(true);
         }
         if (playerShips[1] != null)
         {
             playerShips[1].transform.position = new Vector3(-70, 20);
-            playerShips[3].transform.eulerAngles = new Vector3(0, 0, 45);
+            playerShips[1].transform.eulerAngles = new Vector3(0, 0, 315);
             playerShips[1].SetActive(true);
         }
         if (playerShips[2] != null)
         {
             playerShips[2].transform.position = new Vector3(-100, -20);
-            playerShips[3].transform.eulerAngles = new Vector3(0, 0, 135);
+            playerShips[2].transform.eulerAngles = new Vector3(0, 0, 135);
             playerShips[2].SetActive(true);
         }
         if (playerShips[3] != null)
