06b0784623c60d28b237213f2ee1206a4e59e9f2
Phillip Phoelich <pphoelich@gmail.com>
2015-12-03 16:35:19 +0100

    Readied the overlay for the image with button controls.

--- GameDesignProjectClean/Assets/Scripts/BuilderHandler.cs
+++ GameDesignProjectClean/Assets/Scripts/BuilderHandler.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Linq;
+using GamepadInput;
 
 public class BuilderHandler : MonoBehaviour
 {
     public GameObject[] PlayerAreas;
+    public GameObject OverlayCanvas;
 
     public delegate void PreviousScene();
     private PreviousScene prevScene;
@@ -27,12 +29,16 @@ public class BuilderHandler : MonoBehaviour
         {
             ActivatePlayerAreas(new [] {true, true, true, true});
         }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (OverlayCanvas.activeSelf && AnyButtonPressed())
+        {
+            OverlayCanvas.SetActive(false);
+        }
     }
 
     // Activating the player areas for the players, who joined.
@@ -76,5 +82,77 @@ public class BuilderHandler : MonoBehaviour
             Destroy(ships[playerIndex]);
             ships[playerIndex] = null;
         }
+    }
+
+    private bool AnyButtonPressed()
+    {
+        GamePad.Button? button;
+        GamePad.Trigger? trigger;
+        return AnyButtonPressed(out button, out trigger);
+    }
+
+    private bool AnyButtonPressed(out GamePad.Button? button, out GamePad.Trigger? trigger)
+    {
+        // Trigger pressed threshold.
+        const float triggerThreshold = .5f;
+        // Defaults.
+        button = null;
+        trigger = null;
+        // Check for buttons.
+        if (GamePad.GetButtonDown(GamePad.Button.A, GamePad.Index.Any))
+        {
+            button = GamePad.Button.A;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.B, GamePad.Index.Any))
+        {
+            button = GamePad.Button.B;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Any))
+        {
+            button = GamePad.Button.X;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.Y, GamePad.Index.Any))
+        {
+            button = GamePad.Button.Y;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.Back, GamePad.Index.Any))
+        {
+            button = GamePad.Button.Back;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.Start, GamePad.Index.Any))
+        {
+            button = GamePad.Button.Start;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.LeftShoulder, GamePad.Index.Any))
+        {
+            button = GamePad.Button.LeftShoulder;
+            return true;
+        }
+        if (GamePad.GetButtonDown(GamePad.Button.RightShoulder, GamePad.Index.Any))
+        {
+            button = GamePad.Button.RightShoulder;
+            return true;
+        }
+
+        // Check for triggers. // TODO Right now, if a trigger is held down before this method. It will think it was pressed.
+        if (GamePad.GetTrigger(GamePad.Trigger.LeftTrigger, GamePad.Index.Any) > triggerThreshold)
+        {
+            trigger = GamePad.Trigger.LeftTrigger;
+            return true;
+        }
+        if (GamePad.GetTrigger(GamePad.Trigger.RightTrigger, GamePad.Index.Any) > triggerThreshold)
+        {
+            trigger = GamePad.Trigger.RightTrigger;
+            return true;
+        }
+
+        // No button pressed.
+        return false;
     }
 }
