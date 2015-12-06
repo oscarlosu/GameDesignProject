fba34cda8a554ece73853e683b3677c05e62c065
Phillip Phoelich <pphoelich@gmail.com>
2015-12-06 12:30:50 +0100

    Fixed spawning in the empty level, but the camera doesn't expand... :/

--- GameDesignProjectClean/Assets/Scripts/LevelControllers/EmptyLevelHandler.cs
+++ GameDesignProjectClean/Assets/Scripts/LevelControllers/EmptyLevelHandler.cs
@@ -1,108 +1,56 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Linq;
 
-public partial class EmptyLevelHandler : MonoBehaviour, ILevelHandler {
+public class EmptyLevelHandler : MonoBehaviour, ILevelHandler {
 
-	// Use this for initialization
-	void Start () {
+    // Spawning.
+    public GameObject CorePrefab;
+    public float SpawnDistanceToCenter;
+
+    // Use this for initialization
+    void Start () {
         var gameHandler = GameObject.FindGameObjectWithTag("GameHandler");
         if (gameHandler != null)
         {
             gameHandler.GetComponent<GameHandler>().LevelHandler = this;
             StartLevel(gameHandler.GetComponent<GameHandler>().GetPlayerShips());
         }
+        else
+        {
+            GameObject[] playerShips = new[]
+            {Instantiate(CorePrefab), Instantiate(CorePrefab), Instantiate(CorePrefab), Instantiate(CorePrefab)};
+            StartLevel(playerShips);
+        }
     }
 
     public void StartLevel(GameObject[] playerShips)
     {
-        switch (playerShips.Length)
+        int shipCount = playerShips.Count(s => s != null);
+        int shipsPlaced = 0;
+
+        if (playerShips[0] != null)
         {
-            case 2:
-                if (playerShips[0] != null)
-                {
-                    playerShips[0].transform.position = new Vector3(-15, 15);
-                    playerShips[0].transform.eulerAngles = new Vector3(0, 0, 45);
-                    playerShips[0].SetActive(true);
-                }
-                if (playerShips[1] != null)
-                {
-                    playerShips[1].transform.position = new Vector3(15, -15);
-                    playerShips[1].transform.eulerAngles = new Vector3(0, 0, 225);
-                    playerShips[1].SetActive(true);
-                }
-                break;
-            case 3:
-                if (playerShips[0] != null)
-                {
-                    playerShips[0].transform.position = new Vector3(-15, 15);
-                    playerShips[0].transform.eulerAngles = new Vector3(0, 0, 45);
-                    playerShips[0].SetActive(true);
-                }
-                if (playerShips[1] != null)
-                {
-                    playerShips[1].transform.position = new Vector3(15, 15);
-                    playerShips[1].transform.eulerAngles = new Vector3(0, 0, 315);
-                    playerShips[1].SetActive(true);
-                }
-                if (playerShips[2] != null)
-                {
-                    playerShips[2].transform.position = new Vector3(0, -15);
-                    playerShips[2].transform.eulerAngles = new Vector3(0, 0, 180);
-                    playerShips[2].SetActive(true);
-                }
-                break;
-            case 4:
-                if (playerShips[0] != null)
-                {
-                    playerShips[0].transform.position = new Vector3(-15, 15);
-                    playerShips[0].transform.eulerAngles = new Vector3(0, 0, 45);
-                    playerShips[0].SetActive(true);
-                }
-                if (playerShips[1] != null)
-                {
-                    playerShips[1].transform.position = new Vector3(15, 15);
-                    playerShips[1].transform.eulerAngles = new Vector3(0, 0, 315);
-                    playerShips[1].SetActive(true);
-                }
-                if (playerShips[2] != null)
-                {
-                    playerShips[2].transform.position = new Vector3(-15, -15);
-                    playerShips[2].transform.eulerAngles = new Vector3(0, 0, 135);
-                    playerShips[2].SetActive(true);
-                }
-                if (playerShips[3] != null)
-                {
-                    playerShips[3].transform.position = new Vector3(15, -15);
-                    playerShips[3].transform.eulerAngles = new Vector3(0, 0, 225);
-                    playerShips[3].SetActive(true);
-                }
-                break;
-            default:
-                if (playerShips[0] != null)
-                {
-                    playerShips[0].transform.position = new Vector3(-15, 15);
-                    playerShips[0].transform.eulerAngles = new Vector3(0, 0, 45);
-                    playerShips[0].SetActive(true);
-                }
-                if (playerShips[1] != null)
-                {
-                    playerShips[1].transform.position = new Vector3(15, 15);
-                    playerShips[1].transform.eulerAngles = new Vector3(0, 0, 315);
-                    playerShips[1].SetActive(true);
-                }
-                if (playerShips[2] != null)
-                {
-                    playerShips[2].transform.position = new Vector3(-15, -15);
-                    playerShips[2].transform.eulerAngles = new Vector3(0, 0, 135);
-                    playerShips[2].SetActive(true);
-                }
-                if (playerShips[3] != null)
-                {
-                    playerShips[3].transform.position = new Vector3(15, -15);
-                    playerShips[3].transform.eulerAngles = new Vector3(0, 0, 225);
-                    playerShips[3].SetActive(true);
-                }
-                break;
+            playerShips[0].transform.position = new Vector3(0, SpawnDistanceToCenter);
+            playerShips[0].transform.RotateAround(new Vector3(0, 0, 0), Vector3.forward, (360f / shipCount) * shipsPlaced++ + 45);
+            playerShips[0].SetActive(true);
+        }
+        if (playerShips[1] != null)
+        {
+            playerShips[1].transform.position = new Vector3(0, SpawnDistanceToCenter);
+            playerShips[1].transform.RotateAround(new Vector3(0, 0, 0), Vector3.forward, (360f / shipCount) * shipsPlaced++ + 45);
+            playerShips[1].SetActive(true);
+        }
+        if (playerShips[2] != null)
+        {
+            playerShips[2].transform.position = new Vector3(0, SpawnDistanceToCenter);
+            playerShips[2].transform.RotateAround(new Vector3(0, 0, 0), Vector3.forward, (360f / shipCount) * shipsPlaced++ + 45);
+            playerShips[2].SetActive(true);
+        }
+        if (playerShips[3] != null)
+        {
+            playerShips[3].transform.position = new Vector3(0, SpawnDistanceToCenter);
+            playerShips[3].transform.RotateAround(new Vector3(0, 0, 0), Vector3.forward, (360f / shipCount) * shipsPlaced++ + 45);
+            playerShips[3].SetActive(true);
         }
     }
 }
